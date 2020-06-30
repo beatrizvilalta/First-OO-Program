4e5141a36949d864a422c597499d340ec21907b2
BiaVilalta <beatrizvilaltajimenez@gmail.com>
2020-06-30 17:00:06 -0300

    added constructors

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Products product;
-        product = new Products();
 
         System.out.println("Enter the product data: ");
         System.out.println("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.println("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.println("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Products product = new Products(name, price, quantity);
 
         double totalValue = product.totalValue();
 
@@ -28,7 +28,7 @@ public class Main {
 
         product.addProducts(quantityAdded);
 
-        System.out.println("Updated data: " + product;
+        System.out.println("Updated data: " + product);
 
         System.out.println("Enter the number of product to be removed from stock:");
         int quantityRemoved = sc.nextInt();
--- src/entities/Products.java
+++ src/entities/Products.java
@@ -5,6 +5,12 @@ public class Products {
     public double price;
     public int quantity;
 
+    public Products(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public double totalValue(){
 
         double result = price * quantity;
